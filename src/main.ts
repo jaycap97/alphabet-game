8982a8e254debfde537a92342d187e708e318769
Jascha Pearson <47342646+jaycap97@users.noreply.github.com>
2022-11-26 22:28:30 +0800

    feat(components): add initial values

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,15 @@
-/**
- * main.ts
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
-// Components
 import App from './App.vue'
+import router from './router'
 
-// Composables
 import { createApp } from 'vue'
 
-// Plugins
 import { registerPlugins } from '@/plugins'
 
 const app = createApp(App)
 
+
 registerPlugins(app)
 
-app.mount('#app')
+app
+.use(router)
+.mount('#app')
---
+++ src/views/home/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="d-flex align-center text-center fill-height">
+      <v-img
+        contain
+        height="300"
+        src="src/assets/logo.svg"
+      />
+
+      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+
+      <h1 class="text-h2 font-weight-bold">Dashboard</h1>
+
+      <router-link to="/portal">Portal</router-link>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+  //
+</script>
