e28424efe4ead901a874830b27a356ca4b3ec8b0
Jascha Pearson <47342646+jaycap97@users.noreply.github.com>
2022-11-28 21:49:05 +0800

    feat(descending): add new page

---
+++ src/views/home/Descending.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="d-flex align-center text-center fill-height">
+      <h1 class="text-h2 font-weight-bold">Descending</h1>
+
+      <router-link to="/portal">Portal</router-link>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+  //
+</script>
