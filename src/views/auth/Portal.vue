b7db0f349784703edc53507763f63f35ba294009
Jascha Pearson <47342646+jaycap97@users.noreply.github.com>
2022-11-29 21:37:36 +0800

    feat(portal): add title

--- src/views/auth/Portal.vue
+++ src/views/auth/Portal.vue
@@ -6,6 +6,7 @@
         height="300"
         src="src/assets/logo.svg"
       />
+      <h1>Portal</h1>
       <router-link to="/scoreboard">Scoreboard</router-link>
     </v-responsive>
   </v-container>
