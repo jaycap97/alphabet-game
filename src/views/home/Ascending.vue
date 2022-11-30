023b12e7d381eff02db430840ac29b98e19c0857
Jascha Pearson <47342646+jaycap97@users.noreply.github.com>
2022-11-30 19:38:53 +0800

    feat(views): add text fields

--- src/views/home/Ascending.vue
+++ src/views/home/Ascending.vue
@@ -2,7 +2,9 @@
   <v-container class="fill-height">
     <v-responsive class="d-flex align-center text-center fill-height">
       <h1 class="text-h2 font-weight-bold">Ascending</h1>
-
+      <v-text-field
+        variant="outlined"  
+      ></v-text-field>
       <router-link to="/portal">Portal</router-link>
     </v-responsive>
   </v-container>
--- src/views/home/Descending.vue
+++ src/views/home/Descending.vue
@@ -2,7 +2,9 @@
   <v-container class="fill-height">
     <v-responsive class="d-flex align-center text-center fill-height">
       <h1 class="text-h2 font-weight-bold">Descending</h1>
-
+      <v-text-field
+        variant="outlined"
+      ></v-text-field>
       <router-link to="/portal">Portal</router-link>
     </v-responsive>
   </v-container>
