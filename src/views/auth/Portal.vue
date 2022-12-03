b377c237f647579202f7270a63732f090309f7d4
Jascha Pearson <47342646+jaycap97@users.noreply.github.com>
2022-12-03 22:57:33 +0800

    feat(login): add anonymous login

--- src/views/auth/Portal.vue
+++ src/views/auth/Portal.vue
@@ -1,19 +1,33 @@
 <template>
   <v-container class="fill-height">
     <v-responsive class="d-flex align-center text-center fill-height">
-      <v-img
-        contain
-        height="300"
-        src="src/assets/logo.svg"
-      />
+      <div class="my-3 text-h2 font-weight-bold">Alphabet Game</div>
       <v-text-field
+        class="my-3"
+        label="Type your name"
         variant="outlined"
+        hide-details
       ></v-text-field>
-      <router-link to="/scoreboard">Scoreboard</router-link>
+      <v-btn>
+        Create a room
+      </v-btn>
+      <v-btn>
+        Join a room
+      </v-btn>
     </v-responsive>
   </v-container>
 </template>
 
 <script setup lang="ts">
-  //
+import { onMounted } from 'vue'
+import { auth } from '@/fb'
+import { signInAnonymously } from "firebase/auth"
+
+onMounted(() => {
+  try {
+    signInAnonymously(auth)
+  } catch (err) {
+    console.log(err)
+  }
+})
 </script>
