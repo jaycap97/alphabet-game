694099e9f122c652cb99db613af2b4d59ae98155
Jascha Pearson <47342646+jaycap97@users.noreply.github.com>
2022-12-03 23:03:26 +0800

    refactor(header): add exit room button

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -1,18 +1,21 @@
 <template>
-  <v-app-bar flat>
+  <v-app-bar flat color="secondary">
+    <v-btn dark icon @click="toggleTheme()" class="mx-1">
+      <v-icon>{{ theme.global.current.value.dark? 'mdi-white-balance-sunny' : 'mdi-power-sleep' }}</v-icon>
+    </v-btn>
     <v-spacer />
-    <v-btn dark icon to="/scoreboard" class="mr-1">
+    <v-btn dark icon to="/scoreboard" class="mx-1">
       <v-icon>mdi-counter</v-icon>
     </v-btn>
-    <v-btn dark icon to="/ascending" class="mr-1">
+    <v-btn dark icon to="/ascending" class="mx-1">
       <v-icon>mdi-sort-alphabetical-ascending</v-icon>
     </v-btn>
-    <v-btn dark icon to="/descending" class="mr-1">
+    <v-btn dark icon to="/descending" class="mx-1">
       <v-icon>mdi-sort-alphabetical-descending</v-icon>
     </v-btn>
     <v-spacer />
-    <v-btn dark icon @click="toggleTheme()" class="mr-1">
-      <v-icon>{{ theme.global.current.value.dark? 'mdi-white-balance-sunny' : 'mdi-power-sleep' }}</v-icon>
+    <v-btn dark icon @click="" class="mx-1">
+      <v-icon>mdi-logout-variant</v-icon>
     </v-btn>
   </v-app-bar>
 </template>
