bd8664326abc7cead85af9392705cabcbd3eb8e7
Jascha Pearson <47342646+jaycap97@users.noreply.github.com>
2022-12-06 23:39:18 +0800

    feat(fields): add strict validations

--- src/views/home/Ascending.vue
+++ src/views/home/Ascending.vue
@@ -1,15 +1,40 @@
 <template>
   <v-container class="fill-height">
-    <v-responsive class="d-flex align-center text-center fill-height">
-      <h1 class="text-h2 font-weight-bold">Ascending</h1>
+    <v-row class="mb-5" justify="center" dense>
+      <div class="text-h2 font-weight-bold">{{ alphabet[field.length].toUpperCase() }}</div>
+    </v-row>
+    <v-row class="mb-8" justify="center" dense>
       <v-text-field
-        variant="outlined"  
+        :value="field"
+        @update:model-value="(val: string) => buff = val"
+        variant="outlined"
+        hide-details
       ></v-text-field>
-      <router-link to="/portal">Portal</router-link>
-    </v-responsive>
+      <v-btn class="ml-4" size="x-large">Reset</v-btn>
+    </v-row>
+    <v-row class="mb-8" justify="center" dense>
+      <div class="text-h5">Time: 0.000s</div>
+    </v-row>
+    <v-row justify="center" dense>
+      <AppTimeline />
+    </v-row>
   </v-container>
 </template>
 
 <script setup lang="ts">
-  //
+  import { ref, watch } from 'vue'
+  import AppTimeline from '@/components/AppTimeline.vue'
+
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  const field = ref('')
+  const buff = ref('')
+
+  watch(buff, (newVal) => {
+    // console.log('Fld: ', field.value)
+    // console.log('Bff: ', buff.value)
+    // console.log('Ref: ', alphabet.substring(0, field.value.length + 1))
+    if(alphabet.substring(0, field.value.length + 1) === newVal){
+      field.value = newVal
+    }
+  })
 </script>
--- src/views/home/Descending.vue
+++ src/views/home/Descending.vue
@@ -1,15 +1,23 @@
 <template>
   <v-container class="fill-height">
-    <v-responsive class="d-flex align-center text-center fill-height">
-      <h1 class="text-h2 font-weight-bold">Descending</h1>
+    <v-row class="mb-5" justify="center" dense>
+      <div class="text-h2 font-weight-bold">Z</div>
+    </v-row>
+    <v-row class="mb-8" justify="center" dense>
       <v-text-field
         variant="outlined"
+        hide-details
       ></v-text-field>
-      <router-link to="/portal">Portal</router-link>
-    </v-responsive>
+    </v-row>
+    <v-row class="mb-8" justify="center" dense>
+      <div class="text-h5">Time: 0.000s</div>
+    </v-row>
+    <v-row justify="center" dense>
+      <AppTimeline />
+    </v-row>
   </v-container>
 </template>
 
 <script setup lang="ts">
-  //
+  import AppTimeline from '@/components/AppTimeline.vue'
 </script>
