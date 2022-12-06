6f9283130b1d168cd14f1129a1b70da4c5bac67d
Jascha Pearson <47342646+jaycap97@users.noreply.github.com>
2022-12-06 23:39:45 +0800

    refactor(portal): add temporary dummy data

--- src/views/home/Scoreboard.vue
+++ src/views/home/Scoreboard.vue
@@ -1,13 +1,115 @@
 <template>
   <v-container class="fill-height">
-    <v-responsive class="d-flex align-center text-center fill-height">
-      <h1 class="text-h2 font-weight-bold">Scoreboard</h1>
-
-      <router-link to="/portal">Portal</router-link>
-    </v-responsive>
+    <v-row class="mb-5" justify="center" dense>
+      <div class="text-h2 font-weight-bold">Scoreboard</div>
+    </v-row>
+    <v-row justify="center" dense>
+      <v-card 
+        class="rounded-lg"
+        variant="outlined"
+        max-width="500"
+      >
+        <v-table>
+          <thead>
+            <tr>
+              <th class="text-center">
+                Name
+              </th>
+              <th class="text-center">
+                A to Z
+              </th>
+              <th class="text-center">
+                Z to A
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="item in desserts"
+              :key="item.name"
+            >
+              <td>{{ item.name }}</td>
+              <td>{{ item.az }}</td>
+              <td>{{ item.za }}</td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-card>
+    </v-row>
   </v-container>
 </template>
 
 <script setup lang="ts">
-  //
+  const desserts = [
+    {
+      name: 'Frozen Yogurt',
+      az: 159,
+      za: 159,
+    },
+    {
+      name: 'Ice cream sandwich',
+      az: 237,
+      za: 237,
+    },
+    {
+      name: 'Eclair',
+      az: 262,
+      za: 262,
+    },
+    {
+      name: 'Cupcake',
+      az: 305,
+      za: 305,
+    },
+    {
+      name: 'Gingerbread',
+      az: 356,
+      za: 356,
+    },
+    {
+      name: 'Jelly bean',
+      az: 375,
+      za: 375,
+    },
+    {
+      name: 'Lollipop',
+      az: 392,
+      za: 392,
+    },
+    {
+      name: 'Frozen Yogurt',
+      az: 159,
+      za: 159,
+    },
+    {
+      name: 'Ice cream sandwich',
+      az: 237,
+      za: 237,
+    },
+    {
+      name: 'Eclair',
+      az: 262,
+      za: 262,
+    },
+    {
+      name: 'Cupcake',
+      az: 305,
+      za: 305,
+    },
+    {
+      name: 'Gingerbread',
+      az: 356,
+      za: 356,
+    },
+    {
+      name: 'Jelly bean',
+      az: 375,
+      za: 375,
+    },
+    {
+      name: 'Lollipop',
+      az: 392,
+      za: 392,
+    },
+  ]
 </script>
