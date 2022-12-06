947f9717df96ce0b93d8154f34b6462ce1c92e53
Jascha Pearson <47342646+jaycap97@users.noreply.github.com>
2022-12-06 20:07:37 +0800

    refactor(timeline): move position to start

--- src/components/AppTimeline.vue
+++ src/components/AppTimeline.vue
@@ -1,47 +1,64 @@
 <template>
-  <v-timeline
-    side="end" 
-    align="start"
-    truncate-line="both"
+  <v-card 
+    class="py-5 rounded-lg"
+    variant="outlined"
+    min-width="300"
   >
-    <v-timeline-item
-      dot-color="pink"
-      size="small"
-      icon="mdi-alpha-a"
+    <v-timeline
+      side="end" 
+      truncate-line="both"
     >
-      <div class="d-flex text-center">
-        <strong>0.000s</strong>
-      </div>
-    </v-timeline-item>
+      <v-timeline-item
+        dot-color="pink"
+        size="small"
+        icon="mdi-alpha-a"
+      >
+        <div class="d-flex">
+          <div
+            class="pt-1 headline font-weight-bold"
+            v-text="'0.000s'"
+          ></div>
+        </div>
+      </v-timeline-item>
 
-    <v-timeline-item
-      dot-color="teal-lighten-3"
-      size="small"
-      icon="mdi-alpha-b"
-    >
-      <div class="d-flex text-center">
-        <strong>0.012s</strong>
-      </div>
-    </v-timeline-item>
+      <v-timeline-item
+        dot-color="teal-lighten-3"
+        size="small"
+        icon="mdi-alpha-b"
+      >
+        <div class="d-flex">
+          <div
+            class="pt-1 headline font-weight-bold"
+            v-text="'0.012s'"
+          ></div>
+        </div>
+      </v-timeline-item>
 
-    <v-timeline-item
-      dot-color="pink"
-      size="small"
-      icon="mdi-alpha-c"
-    >
-      <div class="d-flex">
-        <strong>0.024s</strong>
-      </div>
-    </v-timeline-item>
+      <v-timeline-item
+        dot-color="pink"
+        size="small"
+        icon="mdi-alpha-c"
+      >
+        <div class="d-flex">
+          <div
+            class="pt-1 headline font-weight-bold"
+            v-text="'0.024s'"
+          ></div>
+        </div>
+      </v-timeline-item>
 
-    <v-timeline-item
-      dot-color="teal-lighten-3"
-      size="small"
-      icon="mdi-alpha-d"
-    >
-      <div class="d-flex">
-        <strong>0.500s</strong>
-      </div>
-    </v-timeline-item>
-  </v-timeline>
+      <v-timeline-item
+        dot-color="teal-lighten-3"
+        size="small"
+        icon="mdi-alpha-d"
+      >
+        <div class="d-flex">
+          <div
+            class="pt-1 headline font-weight-bold"
+            v-text="'0.500s'"
+          ></div>
+        </div>
+      </v-timeline-item>
+    </v-timeline>
+  </v-card>
 </template>
