2cd650f07520ef02b0a06e4618519c33480fd6d1
Jascha Pearson <47342646+jaycap97@users.noreply.github.com>
2022-12-06 23:40:12 +0800

    refactor(portal): add new buttons

--- src/views/auth/Portal.vue
+++ src/views/auth/Portal.vue
@@ -1,33 +1,54 @@
 <template>
-  <v-container class="fill-height">
-    <v-responsive class="d-flex align-center text-center fill-height">
-      <div class="my-3 text-h2 font-weight-bold">Alphabet Game</div>
-      <v-text-field
-        class="my-3"
-        label="Type your name"
-        variant="outlined"
-        hide-details
-      ></v-text-field>
-      <v-btn>
-        Create a room
-      </v-btn>
-      <v-btn>
-        Join a room
-      </v-btn>
+  <v-container class="align-center fill-height">
+    <v-responsive class="px-5 d-flex align-center text-center fill-height">
+    <div class="mb-10 text-h2 font-weight-bold">Alphabet Game</div>
+    <v-row justify="center">
+      <v-card width="600" variant="flat">
+        <v-container>
+          <v-row class="mb-8">
+          <v-text-field
+            v-model="room_number"
+            :rules="[
+              (v) => !!v || '',
+              (v) => v.length == 4 || ''
+            ]"
+            class="my-3"
+            label="Room Number"
+            variant="outlined"
+            hide-details
+          ></v-text-field>
+        </v-row>
+        <v-row class="mb-8" justify="center">
+          <v-btn :disabled="!room_number || room_number.length != 4" to="/scoreboard">
+            Join a room
+          </v-btn>
+        </v-row>
+        <v-divider class="mb-8" />
+        <v-row class="mb-8" justify="center">
+          <v-btn to="/scoreboard">
+            Create a room
+          </v-btn>
+        </v-row>
+        </v-container>
+      </v-card>
+    </v-row>
     </v-responsive>
   </v-container>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
-import { auth } from '@/fb'
-import { signInAnonymously } from "firebase/auth"
+  import { auth } from '@/fb'
+  import { signInAnonymously } from "firebase/auth"
+  import { ref, onMounted } from 'vue'
 
-onMounted(() => {
-  try {
-    signInAnonymously(auth)
-  } catch (err) {
-    console.log(err)
-  }
-})
+  const room_number = ref('')
+
+  onMounted(() => {
+    try {
+      signInAnonymously(auth)
+      console.log(auth.currentUser)
+    } catch (err) {
+      console.log(err)
+    }
+  })
 </script>
