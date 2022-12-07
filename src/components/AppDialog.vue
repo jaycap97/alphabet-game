d41664531952f58ed5e53b9111e916d271dcc04f
Jascha Pearson <47342646+jaycap97@users.noreply.github.com>
2022-12-07 22:53:51 +0800

    feat(layout): add initial name dialog

---
+++ src/components/AppDialog.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-dialog
+    :model-value="props.toggle"
+    :width="props.width"
+    persistent
+  >
+    <v-card>
+      <v-card-title v-if="props.title" class="font-weight-medium pb-0">{{ props.title }}</v-card-title>
+      <v-card-text class="py-3">
+        <slot />
+      </v-card-text>
+    
+      <v-card-actions>
+        <v-spacer />
+        <v-btn
+          v-for="(action, i) in actions"
+          color="primary"
+          :text="(actions.length === 1)? true : (i !== actions.length - 1)"
+          @click="$emit(action)"
+        >{{ action }}</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>  
+</template>
+
+<script setup lang="ts">
+  import { defineProps } from 'vue'
+
+  const props = defineProps<{
+    toggle: boolean
+    title: string
+    width: string
+    actions: string[]
+  }>()
+</script>
--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -4,9 +4,27 @@
     <v-main>
       <router-view />
     </v-main>
+
+    <!-- Enter a name dialog -->
+    <AppDialog
+      :toggle="toggleDialog"
+      :title="'Name'"
+      :width="'300'"
+      :actions="['cancel', 'enter']"
+      @cancel="toggleDialog = false"
+    >
+      <v-text-field
+        variant="outlined"
+        hide-details
+      ></v-text-field>
+    </AppDialog>
   </v-app>
 </template>
 
 <script setup lang="ts">
   import Header from '@/layouts/Header.vue'
+  import AppDialog from '@/components/AppDialog.vue'
+  import { ref } from 'vue';
+
+  const toggleDialog = ref(true)
 </script>
