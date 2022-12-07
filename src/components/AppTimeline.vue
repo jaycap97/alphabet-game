7a3ee87e7f098c0c6b8407c7673f981f74ab0c21
Jascha Pearson <47342646+jaycap97@users.noreply.github.com>
2022-12-07 22:54:50 +0800

    refactor(timeline): modify the timeline to be dynamic and use props

--- src/components/AppTimeline.vue
+++ src/components/AppTimeline.vue
@@ -9,56 +9,28 @@
       truncate-line="both"
     >
       <v-timeline-item
-        dot-color="pink"
-        size="small"
-        icon="mdi-alpha-a"
-      >
-        <div class="d-flex">
-          <div
-            class="pt-1 headline font-weight-bold"
-            v-text="'0.000s'"
-          ></div>
-        </div>
-      </v-timeline-item>
-
-      <v-timeline-item
+        v-for="i in props.alphabet.length"
         dot-color="teal-lighten-3"
         size="small"
-        icon="mdi-alpha-b"
+        :icon="`mdi-alpha-${props.alphabet[i-1]}`"
       >
         <div class="d-flex">
           <div
             class="pt-1 headline font-weight-bold"
-            v-text="'0.012s'"
-          ></div>
-        </div>
-      </v-timeline-item>
-
-      <v-timeline-item
-        dot-color="pink"
-        size="small"
-        icon="mdi-alpha-c"
-      >
-        <div class="d-flex">
-          <div
-            class="pt-1 headline font-weight-bold"
-            v-text="'0.024s'"
-          ></div>
-        </div>
-      </v-timeline-item>
-
-      <v-timeline-item
-        dot-color="teal-lighten-3"
-        size="small"
-        icon="mdi-alpha-d"
-      >
-        <div class="d-flex">
-          <div
-            class="pt-1 headline font-weight-bold"
-            v-text="'0.500s'"
+            v-text="`${((props.laps[i-1]-props.start)/1000).toFixed(3)}s`"
           ></div>
         </div>
       </v-timeline-item>
     </v-timeline>
   </v-card>
 </template>
+
+<script setup lang="ts">
+  import { defineProps } from 'vue'
+
+  const props = defineProps<{
+    alphabet: string
+    start: number
+    laps: number[]
+  }>()
+</script>
