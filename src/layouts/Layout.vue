b545b8ab3a450a277af3a2045fbb5a2ed2a1e69b
Jascha Pearson <47342646+jaycap97@users.noreply.github.com>
2022-12-09 01:46:11 +0800

    refactor(layout): add function for modifying name

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -14,8 +14,11 @@
       :width="'300'"
       :actions="['cancel', 'enter']"
       @cancel="toggleNameDialog = false"
+      @enter="updateName()"
     >
       <v-text-field
+        v-model="tempName"
+        :rules="[(v) => !!v || '']"
         variant="outlined"
         hide-details
       ></v-text-field>
@@ -28,6 +31,7 @@
       :width="'300'"
       :actions="['cancel', 'proceed']"
       @cancel="toggleExitDialog = false"
+      @proceed="exitRoom()"
     >
       <p>You are about the exit the room.</p>
     </AppDialog>
@@ -37,8 +41,33 @@
 <script setup lang="ts">
   import Header from '@/layouts/Header.vue'
   import AppDialog from '@/components/AppDialog.vue'
-  import { ref } from 'vue';
+  import { ref, watch } from 'vue'
+  import { useStore } from '@/store'
+  import { ActionTypes, MutationTypes } from '@/store/modules/i_users'
+  import router from '@/router'
 
   const toggleNameDialog = ref(false)
   const toggleExitDialog = ref(false)
+  const store = useStore()
+  const name = ref(store.state.name)
+  const tempName = ref('')
+
+  watch(name, () => {
+    if(name.value === '') toggleNameDialog.value = true
+    else toggleNameDialog.value = false
+  },{
+    immediate: true
+  })
+
+  const updateName = () => {
+    toggleNameDialog.value = false
+    store.commit(MutationTypes.SET_NAME, tempName.value)
+    store.dispatch(ActionTypes.UPDATE_CURRENT)
+  } 
+  const exitRoom = () => {
+    toggleExitDialog.value = false
+    store.commit(MutationTypes.SET_ROOM, '')
+    store.dispatch(ActionTypes.UPDATE_CURRENT)
+    router.push({ name: 'Portal' })
+  }
 </script>
